01c42a9a655e3bbaf03ef6ee69af527508558fe3
DeepakSRawat <deepsrawat501@gmail.com>
2021-11-17 17:20:49 +0530

    sone some changes in 3 files

--- emotion_recognition/select_option.py
+++ emotion_recognition/select_option.py
@@ -1,23 +1,28 @@
-import sys
-# Dictionary to create multiple buttons
-values = {"1.click photo" : "1",
-         "2.capture video" : "2",
-         "3.upload photo" : "3",
-         "4.upload video" : "4",
-         }
+from capture_video import cap_v
+from click_photo import clk_phto
+#list to create multiple buttons
+values = ["1.click photo",
+         "2.capture video",
+         "3.upload photo",
+         "4.upload video",
+         ]
 
 def option():
 	n = int(input())
-	if(0<n<5): print(n)
-	else: 
-		print("wrong input")
-		m = str(input("try again or exit:(y/n)"))
-		if(m =='y'or'Y'): option()
-		# if(m=='n'or'N'): sys.exit()
-		else: quit()
+	if(0<n<5): 
+		print(n)
+		if(n==1):clk_phto()
+		if(n==2):cap_v()
+		
+
+	else:
+	    print("wrong input") 
+	    
 #program is starting from here 
 print("select option:\n**********************")
 
+#print values from list 
 for i in values:
 	print(i)
+# calling option:
 option()
